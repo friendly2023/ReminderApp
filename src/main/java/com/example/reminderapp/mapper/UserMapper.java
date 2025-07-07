1a707354f606296659aa8b433fec456b25baf60d
Vladimir <kazimirov.2013@mail.ru>
2025-07-07 03:07:12 +0300

    Add: кеширование паролей юзеров перед сохранением в БД

--- src/main/java/com/example/reminderapp/mapper/UserMapper.java
+++ src/main/java/com/example/reminderapp/mapper/UserMapper.java
@@ -3,10 +3,18 @@ package com.example.reminderapp.mapper;
 import com.example.reminderapp.dto.UserRequestDto;
 import com.example.reminderapp.dto.UserResponseDto;
 import com.example.reminderapp.entity.User;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 @Service
 public class UserMapper {
+
+    @Autowired
+    private PasswordEncoder passwordEncoder;
+
+    public UserMapper() {
+    }
 
     public User toEntity(UserRequestDto dto) {
 
@@ -14,7 +22,7 @@ public class UserMapper {
                 dto.userName(),
                 dto.email(),
                 dto.telegramContact(),
-                dto.password()
+                passwordEncoder.encode(dto.password())
         );
     }
 
