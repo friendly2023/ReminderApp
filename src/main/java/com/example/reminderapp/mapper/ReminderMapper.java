dffba0298607f50f83e29c7262133343b66132d9
Vladimir <kazimirov.2013@mail.ru>
2025-07-23 20:02:44 +0300

    Ref: использован mapstruct для мапинга Reminder

---
+++ src/main/java/com/example/reminderapp/mapper/ReminderMapper.java
@@ -0,0 +1,14 @@
+package com.example.reminderapp.mapper;
+
+import com.example.reminderapp.dto.NewReminderDTO;
+import com.example.reminderapp.entity.Reminder;
+import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
+
+
+@Mapper(componentModel = "spring")
+public interface ReminderMapper {
+    @Mapping(target = "id", ignore = true)
+    @Mapping(target = "user", ignore = true)
+    Reminder toEntity(NewReminderDTO dto);
+}
--- src/main/java/com/example/reminderapp/service/ReminderServiceImpl.java
+++ src/main/java/com/example/reminderapp/service/ReminderServiceImpl.java
@@ -3,6 +3,7 @@ package com.example.reminderapp.service;
 import com.example.reminderapp.dto.NewReminderDTO;
 import com.example.reminderapp.entity.Reminder;
 import com.example.reminderapp.entity.User;
+import com.example.reminderapp.mapper.ReminderMapper;
 import com.example.reminderapp.repository.ReminderRepository;
 import com.example.reminderapp.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
@@ -18,6 +19,7 @@ public class ReminderServiceImpl implements ReminderService {
 
     private final UserRepository userRepository;
     private final ReminderRepository reminderRepository;
+    private final ReminderMapper mapper;
 
     @Override
     public Reminder createReminder(NewReminderDTO newReminderDTO, OAuth2AuthenticationToken auth) {
@@ -31,10 +33,7 @@ public class ReminderServiceImpl implements ReminderService {
         User user = userRepository.findByEmail(email)
                 .orElseThrow(() -> new UsernameNotFoundException("Пользователь с email " + email + " не найден"));
 
-        Reminder newReminder = new Reminder();
-        newReminder.setTitle(newReminderDTO.title());
-        newReminder.setDescription(newReminderDTO.description());
-        newReminder.setRemind(newReminderDTO.remind());
+        Reminder newReminder = mapper.toEntity(newReminderDTO);
         newReminder.setUser(user);
 
         reminderRepository.save(newReminder);
